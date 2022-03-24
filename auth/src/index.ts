5abcc3a92e9184a12ce7cc3158a1b1291ee20129
Lazar Jankovic <jlazar997@gmail.com>
2022-03-24 10:33:48 +0100

    add app file for express

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { NotFoundError } from './errors/not-found-error';
-import { errorHandler } from './middleware/error-handler';
-
-const app = express();
-// traffick is provide through ingress
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(
-	cookieSession({
-		signed: false,
-		secure: true,
-	})
-);
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-//async can wreck havoc
-// to solve it add req, res and next
-// next(new NotFoundError())
-// or even better add express-async-errors package
-app.get('*', async () => {
-	throw new NotFoundError();
-});
-
-//errors must be standardized!
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
 	if (!process.env.JWT_KEY) {
