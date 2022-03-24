27eb1d35fc8e8579c41e7ea960af1b305bc308bd
Lazar Jankovic <jlazar997@gmail.com>
2022-03-24 12:57:27 +0100

    testing current user & global function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,9 +1,13 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import request from 'supertest';
 import mongoose from 'mongoose';
 import { app } from '../app';
 
-let mongo: any;
+declare global {
+	function signin(): Promise<string[]>;
+}
 
+let mongo: any;
 beforeAll(async () => {
 	process.env.JWT_KEY = 'asdasd';
 	mongo = await MongoMemoryServer.create();
@@ -23,3 +27,19 @@ afterAll(async () => {
 	await mongo.stop();
 	await mongoose.connection.close();
 });
+
+global.signin = async () => {
+	const email = 'test@test.com';
+	const password = 'password';
+
+	const response = await request(app)
+		.post('/api/users/signup')
+		.send({
+			email,
+			password,
+		})
+		.expect(201);
+
+	const cookie = response.get('Set-Cookie');
+	return cookie;
+};
