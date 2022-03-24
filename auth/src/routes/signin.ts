5b81acbac5bb4c40d5b7ebd6529ac3ab6637f758
Lazar Jankovic <jlazar997@gmail.com>
2022-03-24 08:11:42 +0100

    signin jwt

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user';
 import { validateRequest } from '../middleware/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -11,8 +15,35 @@ router.post(
 		body('password').trim().notEmpty().withMessage('Password is invalid'),
 	],
 	validateRequest,
-	(req: Request, res: Response) => {
-		res.send('Hi there!');
+	async (req: Request, res: Response) => {
+		const { email, password } = req.body;
+
+		const existingUser = await User.findOne({ email });
+		if (!existingUser) {
+			throw new BadRequestError('Invalid credentials');
+		}
+
+		const passwordMatch = await Password.compare(
+			existingUser.password,
+			password
+		);
+
+		if (!passwordMatch) {
+			throw new BadRequestError('Invalid credentials');
+		}
+
+		const userJwt = jwt.sign(
+			{
+				id: existingUser.id,
+				email: existingUser.email,
+			},
+			process.env.JWT_KEY!
+		);
+
+		req.session = {
+			jwt: userJwt,
+		};
+		res.status(201).send(existingUser);
 	}
 );
 
