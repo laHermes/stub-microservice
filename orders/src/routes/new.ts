ac7172c5d7d9988222e9fdc4fcc229b1890a28d6
Lazar Jankovic <jlazar997@gmail.com>
2022-04-16 14:31:27 +0200

    add ticket and order models

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,47 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import {
+	BadRequestError,
+	NotFoundError,
+	OrderStatus,
+	requireAuth,
+	validateRequest,
+} from '@microstub/common';
+import { body } from 'express-validator';
+
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-	res.send({});
-});
+router.post(
+	'/api/orders',
+	requireAuth,
+	[
+		body('ticketId')
+			.not()
+			.isEmpty()
+			.custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+			.withMessage('Ticket ID must be valid!'),
+	],
+	validateRequest,
+	async (req: Request, res: Response) => {
+		const { ticketId } = req.body;
+
+		const ticket = await Ticket.findById(req.body.ticketId);
+		if (!ticket) {
+			throw new NotFoundError();
+		}
+
+		//this logic is extracted to the ticket model file
+		const isReserved = await ticket.isReserved();
+
+		if (isReserved) {
+			throw new BadRequestError('Ticket is already reserved');
+		}
+
+		res.send({});
+	}
+);
 
 export { router as newOrderRouter };
