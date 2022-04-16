91973cb062fed21c2d5716a54f6f49c06f223b71
Lazar Jankovic <jlazar997@gmail.com>
2022-04-16 16:37:43 +0200

    orders

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,53 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+it('ticket does not exist => error', async () => {
+	const ticketId = new mongoose.Types.ObjectId();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ ticketId })
+		.expect(404);
+});
+
+it('ticket already reserved => error', async () => {
+	const ticket = Ticket.build({
+		title: 'Concert',
+		price: 20,
+	});
+	await ticket.save();
+
+	const order = Order.build({
+		userId: 'asdasd',
+		status: OrderStatus.Created,
+		expiresAt: new Date(),
+		ticket,
+	});
+	await order.save();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ ticketId: ticket.id })
+		.expect(400);
+});
+
+it('reserved a ticket => success', async () => {
+	const ticket = Ticket.build({
+		title: 'Concert',
+		price: 20,
+	});
+	await ticket.save();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ ticketId: ticket.id })
+		.expect(201);
+});
+
+it.todo('emits an order created event');
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -14,6 +14,8 @@ import { Order } from '../models/order';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post(
 	'/api/orders',
 	requireAuth,
@@ -40,7 +42,19 @@ router.post(
 			throw new BadRequestError('Ticket is already reserved');
 		}
 
-		res.send({});
+		const expiration = new Date();
+		expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+		const order = Order.build({
+			userId: req.currentUser!.id,
+			status: OrderStatus.Created,
+			expiresAt: expiration,
+			ticket,
+		});
+
+		await order.save();
+
+		res.status(201).send(order);
 	}
 );
 
